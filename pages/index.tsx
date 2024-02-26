561ba50adae351075ed65fe4f23b08669007e73d
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-02-26 14:16:18 +0100

    wip : add menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import Nav from "@/Components/Nav";
 import React from 'react'; 
 
+
 const HomePage = () => {
-  return <div className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">HomePage</div>;
+  return (
+    <header className="font-medium border-b border-purple-800">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <h1 className="flex flex-col cursor-pointer title-font font-medium">
+          SabrinaSeeks
+          <span className="self-end">Stories</span>
+        </h1>
+        <Nav />
+      </div>
+    </header>
+  );  
 };
 
 export default HomePage;
