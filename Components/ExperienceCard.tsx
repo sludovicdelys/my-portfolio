9d52d2c0451cf12399b116db367c8df33afcfda6
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-04-19 17:06:39 +0200

    fix : update experience cards logos and fix layout

--- Components/ExperienceCard.tsx
+++ Components/ExperienceCard.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
-import nextLogo from "../public/next-logo.svg";
-import typescriptLogo from "../public/ts-logo.svg";
-import symfonyLogo from "../public/symfony-logo.svg";
+import nextLogo from "../public/nextjs-logo.png";
+import typescriptLogo from "../public/ts-logo.png";
+import symfonyLogo from "../public/symfony-logo.png";
 
 type Props = {};
 
@@ -12,26 +12,28 @@ export default function ExperienceCard({}: Props) {
             <div className="px-0 md:px-5">
                 <h4 className="text-2xl md:text-4xl font-light text-center md:mt-16">Gyn&Co</h4>
                 <p className="font-bold text:lg md:text-2xl mt-1 text-center">Volunteer Fullstack Developer</p>
-                <div className="flex space-x-2 my-2 justify-center">
-                    <Image 
-                        src={nextLogo} 
-                        alt="Next.js logo" 
-                        width={100} 
-                        height={100} 
-                        className=""/>
-                    <Image 
-                        src={typescriptLogo} 
-                        alt="TypeScript logo" 
-                        width={100} 
-                        height={100} 
-                        className=""/>
-                    <Image 
-                        src={symfonyLogo} 
-                        alt="Symfony logo" 
-                        width={100} 
-                        height={100}
-                        className=""/>
-                </div>
+                <ul className="flex space-x-2 my-2 justify-center">
+                    <li className="flex justify-center items-center">
+                        <Image 
+                            src={nextLogo} 
+                            alt="Next.js logo" 
+                            className="object-contain h-[70px] w-[70px]"/>
+                    </li>
+                    
+                    <li className="flex justify-center items-center">
+                        <Image 
+                            src={typescriptLogo} 
+                            alt="TypeScript logo" 
+                            className="object-contain h-[70px] w-[70px]"/>
+                    </li>
+                    
+                    <li className="flex justify-center items-center">
+                        <Image 
+                            src={symfonyLogo} 
+                            alt="Symfony logo" 
+                            className="object-contain h-[70px] w-[70px]"/>
+                    </li>
+                </ul>
                 <p className="uppercase py-5 text-gray-500 text-xs md:text-xl text-center">Started on Oct 2022 - Current</p>
                 <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg w-5/6 text-center md:text-left md:w-full">
                     <li>Contributed to the design and implementation of layered architecture.</li>
--- Components/ExperienceCardTwo.tsx
+++ Components/ExperienceCardTwo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import scssLogo from "../public/scss-logo.png";
-import figmaLogo from "../public/figma-logo.svg";
+import figmaLogo from "../public/figma-logo.png";
 import twigLogo from "../public/twig-logo.png";
 
 type Props = {};
@@ -12,23 +12,26 @@ export default function ExperienceCardTwo({}: Props) {
             <div className="px-0 md:px-5">
                 <h4 className="text-2xl md:text-4xl font-light text-center md:mt-16">Elao</h4>
                 <p className="font-bold text:lg md:text-2xl mt-1 text-center">Web Integrator</p>
-                <div className="flex space-x-2 my-2 justify-center">
-                    <Image 
-                        src={twigLogo} 
-                        alt="Twig logo" 
-                        height={50} 
-                        className=""/>
-                    <Image 
-                        src={scssLogo} 
-                        alt="Sass logo"
-                        height={50} 
-                        className=""/>
-                    <Image 
-                        src={figmaLogo} 
-                        alt="Figma logo" 
-                        height={50}
-                        className=""/>
-                </div>
+                <ul className="flex space-x-2 my-2 justify-center">
+                    <li className="flex justify-center items-center">
+                        <Image 
+                            src={twigLogo} 
+                            alt="Twig logo" 
+                            className="object-contain h-[70px] w-[70px]"/>
+                    </li>
+                    <li className="flex justify-center items-center">
+                        <Image 
+                            src={scssLogo} 
+                            alt="Sass logo" 
+                            className="object-contain h-[70px] w-[70px]"/>
+                    </li>
+                    <li className="flex justify-center items-center">
+                        <Image 
+                            src={figmaLogo} 
+                            alt="Figma logo"
+                            className="object-contain h-[70px] w-[70px]"/>
+                    </li> 
+                </ul>
                 <p className="uppercase py-5 text-gray-500 text-xs md:text-xl text-center">From Aug 2021 - To Feb 2023</p>
                 <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg w-5/6 text-center md:text-left md:w-full">
                     <li>Cutting and integrating graphic mockups using Figma.</li>
